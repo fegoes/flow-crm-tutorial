a8fb4fc50ed95d4757659b23b6d5961ce8663f76
start.vaadin.com <no-reply@vaadin.com>
2021-12-01 19:47:39 +0000

    Generated project

---
+++ src/main/java/com/rains/views/list/ListView.java
@@ -0,0 +1,33 @@
+package com.rains.views.list;
+
+import com.rains.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("list")
+@Route(value = "hello", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class ListView extends VerticalLayout {
+
+    public ListView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
